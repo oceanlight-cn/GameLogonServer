6f4de03debcc6e7128e3f4e629c9cc82826943cc
50129815 <172313193@qq.com>
2020-06-30 10:45:39 +0800

    implement db query function.

---
+++ src/db_oper.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	 "database/sql"
+    _ "github.com/go-sql-driver/mysql"
+)
+
+const (
+	_selectUser = "select count(*) from users where name = ? and password = ?"
+)
+
+func validate_user(name string, password string) bool{
+	var (
+    	err error
+    )
+
+    db,err := sql.Open("mysql","root:123456@tcp(127.0.0.1:3306)/game_server")
+    if err != nil {
+        fmt.Println(err)
+        return false
+    }
+    defer db.Close()
+
+    fmt.Println("db connected.")
+
+    var (
+    	user_count int
+    )
+    err = db.QueryRow(_selectUser,name, password).Scan(&user_count)
+    if err != nil {
+        fmt.Println(err)
+        return false
+    }
+    //fmt.Println(user_count)
+    if user_count == 1 {
+    	return true
+    } else {
+    	return false
+    }
+}
--- src/server.go
+++ src/server.go
@@ -2,12 +2,50 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"net"
+	"strings"
 )
 
 func logon(c net.Conn, inputText string) {
 	// /login?user=xx&password=pwd
+	result_str := "login fail.\n"
+	var (
+		user string
+		password string
+	)
+
+	if strings.Contains(inputText, "login"){
+		pos_start := strings.Index(inputText, "user")
+		if pos_start != -1 {
+			pos_end := strings.Index(inputText, "&")
+			if pos_end != -1 {
+				user = inputText[pos_start+5:pos_end]
+				fmt.Println("user:", user)
+			} else {
+				fmt.Fprintf(c, result_str)
+				return
+			}
+		} else {
+			fmt.Fprintf(c, result_str)
+		}
+
+		pos_start = strings.Index(inputText, "password")
+		if pos_start != -1 {
+			password = inputText[pos_start+9:len(inputText)]
+			fmt.Println("password:", password)
+			if validate_user(user, password) {
+				result_str = "login ok.\n"
+			}
+			fmt.Fprintf(c, result_str)
+		} else {
+			fmt.Fprintf(c, result_str)
+		}
+
+	} else {
+		fmt.Fprintf(c, result_str)
+	}
 }
 
 //!+
