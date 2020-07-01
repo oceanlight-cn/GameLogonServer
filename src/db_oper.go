0d64160155014d4f3721f1a12c2e1ab6bbe86236
50129815 <172313193@qq.com>
2020-07-01 09:40:45 +0800

    implement concurrency.

--- src/db_oper.go
+++ src/db_oper.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	 "database/sql"
     _ "github.com/go-sql-driver/mysql"
+    "sync"
 )
 
 const (
@@ -12,9 +13,13 @@ const (
 
 func validate_user(name string, password string) bool{
 	var (
+		mu sync.Mutex 
     	err error
     )
 
+	mu.Lock()
+	defer mu.Unlock()
+	
     db,err := sql.Open("mysql","root:123456@tcp(127.0.0.1:3306)/game_server")
     if err != nil {
         fmt.Println(err)
